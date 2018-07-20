bd740dcb73802914dbe47cff29173ee2b1eff4b6
Aredhele <vincentstehly@hotmail.fr>
2018-07-21 01:52:34 +0200

    Add renderer config

---
+++ Engine/Header/Render/Config.hpp
@@ -0,0 +1,13 @@
+/// \file       Config.hpp
+/// \date       21/07/2018
+/// \project    OOM-Engine
+/// \package    Render
+/// \author     Vincent STEHLY--CALISTO
+
+#ifndef OOM_ENGINE_CONFIG_HPP__
+#define OOM_ENGINE_CONFIG_HPP__
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#endif // !OOM_ENGINE_CONFIG_HPP__
