4bfd41b34104c35c59f8dbcf6b73a7fc374382bd
Aredhele <vincentstehly@hotmail.fr>
2018-07-22 02:32:19 +0200

    Add basic object class

---
+++ Engine/Header/Composite/CObject.hpp
@@ -0,0 +1,24 @@
+/// \file       CObject.hpp
+/// \date       22/07/2018
+/// \project    OOM-Engine
+/// \package    Composite
+/// \author     Vincent STEHLY--CALISTO
+
+#ifndef OOM_ENGINE_C_OBJECT_HPP__
+#define OOM_ENGINE_C_OBJECT_HPP__
+
+#include "Core/Standard/CString.hpp"
+
+namespace Oom
+{
+
+class CObject
+{
+public:
+
+    CString name;
+};
+
+}
+
+#endif // !OOM_ENGINE_C_OBJECT_HPP__
---
+++ Engine/Source/Composite/CObject.cpp
@@ -0,0 +1,12 @@
+/// \file       CObject.cpp
+/// \date       22/07/2018
+/// \project    OOM-Engine
+/// \package    Composite
+/// \author     Vincent STEHLY--CALISTO
+
+#include "Composite/CObject.hpp"
+
+namespace Oom
+{
+    // None
+}
