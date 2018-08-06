cf623932f446cd47eca45cd4cc260055a2bae2be
Darenn Keller <keller.darenn@gmail.com>
2018-08-06 20:33:39 +0200

    Created assets prefabs.

--- Engine/Header/Game/Prefabs.hpp
+++ Engine/Header/Game/Prefabs.hpp
@@ -1,7 +1,16 @@
 #include "SDK/SDK.hpp"
+#include "Game/Asset.hpp"
 
 namespace Game::Prefabs {
 	
 	CGameObject& CreatePlayer();
+	
+	CGameObject& CreateCB_Asset(Asset::Size size);
+	CGameObject& CreateRoadAsset(Asset::Size size);
+	CGameObject& CreateBigAsset();
+}
 
+namespace Game::Prefabs::priv
+{
+	CGameObject& CreateAsset(Asset::Size size);
 }
--- Engine/Source/Game/Prefabs.cpp
+++ Engine/Source/Game/Prefabs.cpp
@@ -2,7 +2,7 @@
 
 CGameObject& Game::Prefabs::CreatePlayer() 
 {
-	CGameObject& player = *CEngine::Instantiate();
+	auto& player = *CEngine::Instantiate();
 
 	// Add the camera component
 	bool b_first_camera = (CEngine::FindWithTag("MainCamera") == nullptr);
@@ -16,4 +16,45 @@ CGameObject& Game::Prefabs::CreatePlayer()
 
 
 	return player;
+}
+
+CGameObject& Game::Prefabs::CreateCB_Asset(Asset::Size size)
+{
+	auto& go = Game::Prefabs::priv::CreateAsset(size);
+	return go;
+
+}
+
+CGameObject& Game::Prefabs::CreateRoadAsset(Asset::Size size)
+{
+	auto& go = Game::Prefabs::priv::CreateAsset(size);
+	return go;
+}
+
+CGameObject& Game::Prefabs::CreateBigAsset()
+{
+	auto& go = Game::Prefabs::priv::CreateAsset(Asset::Size::Heavy);
+	return go;
+}
+
+CGameObject& Game::Prefabs::priv::CreateAsset(Asset::Size size)
+{
+	auto& go = *CEngine::Instantiate();
+	auto& asset = *go.AddComponent<Game::Asset>();
+	go.AddComponent<CRigidBody>();
+	go.AddComponent<CBoxCollider>();
+	asset.init(size);
+	switch(size)
+	{
+	case Asset::Size::Light:
+		go.GetTransform().SetScale(1, 1, 1);
+		break;
+	case Asset::Size::Standard:
+		go.GetTransform().SetScale(1.5, 1.5, 1.5);
+		break;
+	case Asset::Size::Heavy:
+		go.GetTransform().SetScale(2, 2, 2);
+		break;
+	}	
+	return go;
 }
